ee045db703ac9cf48ccacdf71d042662c3fffc75
dasl <dasl-@users.noreply.github.com>
2020-12-02 19:50:04 -0500

    2020 update

--- macbook_scraper.php
+++ macbook_scraper.php
@@ -1,20 +1,20 @@
 <?php
 
-const URL = "http://www.apple.com/shop/browse/home/specialdeals/mac/macbook_air/13";
-const MATCH_EMAILS = "david.leibovic@gmail.com,michellebae00@gmail.com";
+const URL = "https://www.apple.com/shop/refurbished/mac/13-inch-macbook-air";
+const MATCH_EMAILS = "david.leibovic@gmail.com";
 const ERROR_EMAILS = "david.leibovic@gmail.com";
 $already_seen_urls = [];
 
 while (true) {
     try {
         $doc = getDomDocument();
-        $match_urls = searchForMatches($doc);
-        $new_matches = removeAlreadySeenMatches($match_urls, $already_seen_urls);
-        if ($new_matches) {
-            echo "Found " . count($new_matches) . " matches: " .  implode(" ", $new_matches) . "\n";
-            sendMatchEmail($new_matches);
-            foreach ($new_matches as $match) {
-                $already_seen_urls[$match] = true;
+        $match_urls_to_desc_map = searchForMatches($doc);
+        $new_match_urls_to_desc_map = removeAlreadySeenMatches($match_urls_to_desc_map, $already_seen_urls);
+        if ($new_match_urls_to_desc_map) {
+            echo "Found " . count($new_match_urls_to_desc_map) . " matches: " .  print_r($new_match_urls_to_desc_map, true) . "\n";
+            sendMatchEmail($new_match_urls_to_desc_map);
+            foreach ($new_match_urls_to_desc_map as $url => $desc) {
+                $already_seen_urls[$url] = true;
             }
         } else {
             echo "No matches found.\n";
@@ -56,21 +56,34 @@ function getDomDocument() {
 
 function searchForMatches($doc) {
     $xpath = new DOMXPath($doc);
-    $spec_box_nodes = $xpath->query("//div[@class='box-content']//td[@class='specs']");
-    echo "Found {$spec_box_nodes->length} results to search through.\n";
-    $match_urls = [];
-    foreach ($spec_box_nodes as $spec_box_node) {
-        $parent_node = $spec_box_node->parentNode;
+    $result_nodes = $xpath->query("//div[@class='refurbished-category-grid-no-js']//li");
+    echo "Found {$result_nodes->length} results to search through.\n";
+    $match_urls_to_desc_map = [];
+    foreach ($result_nodes as $result_node) {
+        $description_nodes = (new DOMXPath($result_node->ownerDocument))->query(".//h3", $result_node);
+        if ($description_nodes->length === 0) {
+            continue;
+        }
+
+        // filter for macbook air results
+        if (stripos($description_nodes->item(0)->nodeValue, 'air') === false) {
+            continue;
+        }
+
+        $url = getUrlForSpecBoxNode($result_node);
+        $match_urls_to_desc_map[$url] = trim($description_nodes->item(0)->nodeValue);
+        continue;
+
         $price_nodes = (new DOMXPath($parent_node->ownerDocument))->query(".//span[@itemprop='price']", $parent_node);
         if ($price_nodes->length !== 1) {
             throw new Exception("Unexpected price nodes count for matches: $price_nodes->length.");
         }
         $price = trim($price_nodes->item(0)->nodeValue);
         $price = (int) str_replace(["$", ","], ["", ""], $price);
-        if ($price < 800) {
+        // if ($price < 800) {
             $match_urls[] = getUrlForSpecBoxNode($spec_box_node);
             continue;
-        }
+        // }
 
         $memory_match_result = 0;
         $storage_match_result = 0;
@@ -97,7 +110,7 @@ function searchForMatches($doc) {
             }
         }
     }
-    return $match_urls;
+    return $match_urls_to_desc_map;
 }
 
 function getUrlForSpecBoxNode($spec_box_node) {
@@ -108,23 +121,26 @@ function getUrlForSpecBoxNode($spec_box_node) {
     return "http://www.apple.com" . $url_nodes->item(0)->getAttribute('href');
 }
 
-function removeAlreadySeenMatches($match_urls, $already_seen_urls) {
-    $new_matches = [];
-    foreach ($match_urls as $match_url) {
-        if (!isset($already_seen_urls[$match_url])) {
-            $new_matches[] = $match_url;
+function removeAlreadySeenMatches($match_urls_to_desc_map, $already_seen_urls) {
+    $new_match_urls_to_desc_map = [];
+    foreach ($match_urls_to_desc_map as $url => $desc) {
+        if (!isset($already_seen_urls[$url])) {
+            $new_match_urls_to_desc_map[$url] = $desc;
         } else {
-            echo "Removing $match_url because it was already seen.\n";
+            echo "Removing $url because it was already seen.\n";
         }
     }
-    return $new_matches;
+    return $new_match_urls_to_desc_map;
 }
 
-function sendMatchEmail($match_urls) {
+function sendMatchEmail($new_match_urls_to_desc_map) {
     $to = MATCH_EMAILS;
     $subject = "omfg we found a macbook air for you!!!!!!!!!";
-    $message = "URL(s): " . implode("\n", $match_urls) . "\n" .
-        "We found these matches at " . URL . "\n" .
+    $message = "URL(s):\n";
+    foreach ($new_match_urls_to_desc_map as $url => $desc) {
+        $message .= $desc . "\n" . $url . "\n\n";
+    }
+    $message .= "We found these matches at " . URL . "\n" .
         "Make sure the price is below the list price of $999.00\n";
 
     if (!mail($to, $subject, $message)) {
